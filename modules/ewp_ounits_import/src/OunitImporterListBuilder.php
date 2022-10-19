c096ad126e06e365b7a8bfc4cc8bbdae7221aa1a
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-10-19 18:30:18 +0200

    New submodule with an importer config entity

---
+++ modules/ewp_ounits_import/src/OunitImporterListBuilder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\ewp_ounits_import;
+
+use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Provides a listing of organizational unit importers.
+ */
+class OunitImporterListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header['label'] = $this->t('Label');
+    $header['id'] = $this->t('Machine name');
+    $header['status'] = $this->t('Status');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    /** @var \Drupal\ewp_ounits_import\OunitImporterInterface $entity */
+    $row['label'] = $entity->label();
+    $row['id'] = $entity->id();
+    $row['status'] = $entity->status() ? $this->t('Enabled') : $this->t('Disabled');
+    return $row + parent::buildRow($entity);
+  }
+
+}
