744e2ca874e24c22ab8b6b66b0e5f7b8f50a53b3
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-10-20 18:36:02 +0200

    Preliminary dependency injection in PreviewForm

--- modules/ewp_ounits_get/src/Form/OunitProviderPreviewForm.php
+++ modules/ewp_ounits_get/src/Form/OunitProviderPreviewForm.php
@@ -4,6 +4,7 @@ namespace Drupal\ewp_ounits_get\Form;
 
 use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Organizational Unit provider preview form.
@@ -11,6 +12,32 @@ use Drupal\Core\Form\FormStateInterface;
  * @property \Drupal\ewp_ounits_get\OunitProviderInterface $entity
  */
 class OunitProviderPreviewForm extends EntityForm {
+
+  /**
+   * A logger instance.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * The messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    $instance = parent::create($container);
+    $instance->loggerFactory        = $container->get('logger.factory');
+    $instance->logger = $instance->loggerFactory->get('ewp_ounits_get');
+    $instance->messenger            = $container->get('messenger');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -22,6 +49,15 @@ class OunitProviderPreviewForm extends EntityForm {
     dpm($this);
 
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function actionsElement(array $form, FormStateInterface $form_state) {
+    $element = [];
+
+    return $element;
   }
 
   /**
